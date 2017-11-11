99a8186ce395f1abe5eec083098b20d8969b4248
Guilherme Rossato <guilhermrossato@gmail.com>
2017-11-11 18:04:10 -0200

    Added events handling to the interface

--- Script/Modules/Interface.js
+++ Script/Modules/Interface.js
@@ -1,5 +1,5 @@
 const Interface = (function() {
-	var self;
+	var self, currentScreen;
 	function init(wrapper) {
 		self = Interface;
 		self.wrapper = (typeof wrapper === "string")?document.getElementById(wrapper):wrapper;
@@ -9,53 +9,63 @@ const Interface = (function() {
 		self.ctx = self.canvas.getContext("2d");
 		self.wrapper.appendChild(self.canvas);
 		
-		self.wrapper.setStyle = (style) => self.wrapper.setAttribute("style", Interface.minifyStyle(style));
+		self.wrapper.setStyle = (style) => self.wrapper.setAttribute("style", minifyStyle(style));
 		self.canvas.setSize = (width, height) => (self.canvas.width = width)&&(self.canvas.height = height);
 		
-		self.setCurrentScreen({});
+		self.setCurrentScreen(ViewPrimitive);
 		[
-			["keydown", self.onRawKeyDown],
-			["keyup", self.onRawKeyUp],
-			["mousedown", self.onRawMouseDown],
-			["mousemove", self.onRawMouseMove],
-			["mouseup", self.onRawMouseUp],
-		].forEach(c => window.addEventListener(c[0], c[1].bind(self)));
+			["keydown", onRawKeyDown],
+			["keyup", onRawKeyUp],
+			["mousedown", onRawMouseDown],
+			["mousemove", onRawMouseMove],
+			["mouseup", onRawMouseUp],
+			["resize", onRawResize],
+		].forEach(c => addEventListener(c[0], c[1]));
 	}
 	function onRawKeyDown(event) {
-		(self.currentScreen.onKeyDown) && self.currentScreen.onKeyDown(event.key);
+		(currentScreen.onKeyDown) && currentScreen.onKeyDown(event.key);
 	}
 	function onRawKeyUp(event) {
-		(self.currentScreen.onKeyUp) && self.currentScreen.onKeyUp(event.key);
+		(currentScreen.onKeyUp) && currentScreen.onKeyUp(event.key);
 	}
 	function onRawMouseDown(event) {
-		(self.currentScreen.onMouseDown) && self.currentScreen.onMouseDown(event.button, event.clientX - self.canvas.offsetLeft, event.clientY - self.canvas.offsetTop);
+		(currentScreen.onMouseDown) && currentScreen.onMouseDown(event.button, event.clientX - self.canvas.offsetLeft, event.clientY - self.canvas.offsetTop);
 	}
 	function onRawMouseMove(event) {
-		(self.currentScreen.onMouseMove) && self.currentScreen.onMouseMove(event.clientX - self.canvas.offsetLeft, event.clientY - self.canvas.offsetTop);
+		(currentScreen.onMouseMove) && currentScreen.onMouseMove(event.clientX - self.canvas.offsetLeft, event.clientY - self.canvas.offsetTop);
 	}
 	function onRawMouseUp(event) {
-		(self.currentScreen.onMouseUp) && self.currentScreen.onMouseUp(event.button, event.clientX - self.canvas.offsetLeft, event.clientY - self.canvas.offsetTop);
+		(currentScreen.onMouseUp) && currentScreen.onMouseUp(event.button, event.clientX - self.canvas.offsetLeft, event.clientY - self.canvas.offsetTop);
+	}
+	function onRawResize(event) {
+		(currentScreen.onResize) && currentScreen.onResize(window.innerWidth, window.innerHeight);
 	}
 	function setCurrentScreen(object) {
 		if (!object)
 			throw new Error("No screen object given");
-		self.currentScreen.close();
-		self.currentScreen = object;
-		self.currentScreen.open(wrapper, canvas, ctx);
+		if (currentScreen)
+			currentScreen.close();
+		currentScreen = object;
+		object.open(self.wrapper, self.canvas);
+		object.draw(self.ctx);
 	}
-	function update() {
-		self.currentScreen.update(delta);
-		self.currentScreen.draw(self.ctx);
+	function update(delta) {
+		currentScreen.update(delta);
+		currentScreen.draw(self.ctx);
 	}
-	static minifyStyle(s, mode=0) {
+	function minifyStyle(s, mode=0) {
 		return s.replace(/(\r\n|\t|\n|\r)/gm,"").split('').filter((char, index) => (((((mode == 1)&&(mode = 2))||true)&&(((char == ':')&&(mode = 1))||true)&&(char == ';')&&(mode = 0)||true)&&mode == 0)?(char !== ' '):((mode == 2)?((mode = 3)&&(char != ' ')):true)).join('');
+	}
+	function sendPause() {
+		currentScreen.pause && currentScreen.pause();
+	}
+	function sendResume() {
+		currentScreen.resume && currentScreen.resume();
 	}
 	return {
 		init: init,
 		setCurrentScreen: setCurrentScreen,
 		update: update,
-		minifyStyle: function(s, mode=0) {
-			return s.replace(/(\r\n|\t|\n|\r)/gm,"").split('').filter((char, index) => (((((mode == 1)&&(mode = 2))||true)&&(((char == ':')&&(mode = 1))||true)&&(char == ';')&&(mode = 0)||true)&&mode == 0)?(char !== ' '):((mode == 2)?((mode = 3)&&(char != ' ')):true)).join('');
-		}
+		minifyStyle: minifyStyle
 	}
 })();
