6c9482d0143f3816ec9ec0fd2ec37ec9843c780f
Guilherme Rossato <guilhermrossato@gmail.com>
2017-11-11 18:03:17 -0200

    Made Application into a proper Module

--- Script/Modules/Application.js
+++ Script/Modules/Application.js
@@ -1,6 +1,26 @@
-class Application {
-	constructor() {
-		this.interface = new Interface("center");
-		VariableTimeStep.begin(this.interface.update.bind(this.interface), this.interface.pause.bind(this.interface));
+const Application = (function() {
+	var self;
+	function init() {
+		self = Application;
+		Interface.init("center");
+		Interface.setCurrentScreen(StartView);
+		VariableTimeStep.begin(Interface.update.bind(Interface), pause);
 	}
-}
+	function pause() {
+		if (self.paused) return;
+		Interface.sendPause();
+		(self.currentScreen.pause) && self.currentScreen.pause();
+		self.paused = true;
+	}
+	function unpause() {
+		if (!self.paused) return;
+		Interface.sendResume();
+		(self.currentScreen.unpause) && self.currentScreen.unpause();
+		self.paused = false;
+	}
+	return {
+		init: init,
+		pause: pause,
+		unpause: unpause
+	}
+})();
