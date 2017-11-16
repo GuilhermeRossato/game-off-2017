8a6b83f1a97bf1b4ac25560531e24ac521284c78
Guilherme Rossato <guilhermrossato@gmail.com>
2017-11-16 21:21:14 -0200

    Added basic state saving for time rewind mechanism
    Still need to implement the fast addition method fully, that's just a placeholder.

--- Script/Classes/Entity.js
+++ Script/Classes/Entity.js
@@ -1,6 +1,25 @@
+const DataInState = 3;
+const HistoryPerFrame = 3;
+const SecondsToSave = 2;
+const TotalToSave = DataInState * HistoryPerFrame * SecondsToSave;
+
 class Entity {
 	constructor(polygons) {
-		this.origin = [0, 0];
+		this.state = new ArrayBuffer(DataInState);
+		this.past = new ArrayBuffer(TotalToSave);
+		this.head = 0;
+		this.tail = 0;
+		this.length = 0;
 		this.polygons = polygons;
+	}
+	updateState(nx, ny, ns) {
+		this.addState(nx, ny, ns);
+		if (this.length < TotalToSave) {
+			this.past[this.length*HistoryPerFrame] = nx;
+			this.past[this.length*HistoryPerFrame+1] = ny;
+			this.past[this.length*HistoryPerFrame+2] = ns;
+			this.tail = this.length;
+			this.length ++;
+		}
 	}
 }
