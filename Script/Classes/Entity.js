d77acd956a8daf6a8247b55ce365045dd4b86ad8
Guilherme Rossato <guilhermrossato@gmail.com>
2017-11-18 22:11:35 -0200

    Transformed Camera a child of Entity
    So not to write duplicated code

--- Script/Classes/Entity.js
+++ Script/Classes/Entity.js
@@ -5,19 +5,18 @@ const TotalToSave = DataInState * HistoryPerFrame * SecondsToSave;
 
 class Entity {
 	constructor(polygons) {
-		this.state = new ArrayBuffer(DataInState);
-		this.past = new ArrayBuffer(TotalToSave);
+		this.state = new Float32Array(DataInState);
+		this.past = new Float32Array(TotalToSave);
 		this.head = 0;
 		this.tail = 0;
 		this.length = 0;
 		this.polygons = polygons;
 	}
-	updateState(nx, ny, ns) {
-		this.addState(nx, ny, ns);
+	updateState() {
 		if (this.length < TotalToSave) {
-			this.past[this.length*HistoryPerFrame] = nx;
-			this.past[this.length*HistoryPerFrame+1] = ny;
-			this.past[this.length*HistoryPerFrame+2] = ns;
+			this.past[this.length*HistoryPerFrame+0] = this.state[0];
+			this.past[this.length*HistoryPerFrame+1] = this.state[1];
+			this.past[this.length*HistoryPerFrame+2] = this.state[2];
 			this.tail = this.length;
 			this.length ++;
 		}
