e641ee5c38d72878124411af2c97c942afdcdf2d
Guilherme Rossato <guilhermrossato@gmail.com>
2017-11-25 01:04:04 -0200

    Finished RoundRobin Rewind Memory for entities
    The very efficient way to save states (at least, that I know of)

--- Script/Classes/Entity.js
+++ Script/Classes/Entity.js
@@ -1,7 +1,7 @@
 const DataInState = 3;
-const HistoryPerFrame = 3;
+const StatePerSecond = 3;
 const SecondsToSave = 2;
-const TotalToSave = DataInState * HistoryPerFrame * SecondsToSave;
+const TotalToSave = DataInState * StatePerSecond * SecondsToSave;
 
 class Entity {
 	constructor(polygons) {
@@ -14,11 +14,17 @@ class Entity {
 	}
 	updateState() {
 		if (this.length < TotalToSave) {
-			this.past[this.length*HistoryPerFrame+0] = this.state[0];
-			this.past[this.length*HistoryPerFrame+1] = this.state[1];
-			this.past[this.length*HistoryPerFrame+2] = this.state[2];
+			this.past[this.length*StatePerSecond+0] = this.state[0];
+			this.past[this.length*StatePerSecond+1] = this.state[1];
+			this.past[this.length*StatePerSecond+2] = this.state[2];
 			this.tail = this.length;
 			this.length ++;
+		} else {
+			this.past[this.head*StatePerSecond+0] = this.state[0];
+			this.past[this.head*StatePerSecond+1] = this.state[1];
+			this.past[this.head*StatePerSecond+2] = this.state[2];
+			this.head = (this.head+1)%TotalToSave;
+			this.tail = (this.tail+1)%TotalToSave;
 		}
 	}
 }
